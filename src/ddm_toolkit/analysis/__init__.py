8148de859cedaab6cb194be983513ff60c5d3ca4
Martinus Werts <martinus.werts@univ-angers.fr>
2026-07-09 15:26:33 +0200

    refresh analysis module

--- src/ddm_toolkit/analysis/__init__.py
+++ src/ddm_toolkit/analysis/__init__.py
@@ -1,3 +1,3 @@
-from .ISFanalysis_simple_brownian import ISFanalysis_simple_brownian
+from .ISF_analysis_simple_brownian import ISFanalysis_simple_brownian
 from .ISF_analysis_simple_brownian_result import ISFanalysis_simple_brownian_result
 
